2929f6a8acc268b042ccb70ebe41ea9538ab39ba
Tramio <tramiodev@outlook.fr>
2021-07-20 16:29:44 +0200

    Add IF block with SWITCH statement for playerSelection == paper and playerSelection = scissors

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function playRound() {
     let defaultMessage = "I don't think I understood well, would you mind telling me again?"
 
     console.log("You went for " + playerSelection + ". Wise choice.","\n","Your opponent chose " + computerSelection + "! What a surprise!!!");
-    
+
     if (playerSelection == "rock") {
         switch (computerSelection) {
         case "rock":
@@ -28,6 +28,36 @@ function playRound() {
             break;
         case "scissors":
             console.log(winMessage);
+            break;
+        default:
+            console.log(defaultMessage);
+            break;
+    }}
+    if (playerSelection == "paper") {
+        switch (computerSelection) {
+        case "rock":
+            console.log(winMessage);
+            break;
+        case "paper":
+            console.log(equaMessage);
+            break;
+        case "scissors":
+            console.log(loseMessage);
+            break;
+        default:
+            console.log(defaultMessage);
+            break;
+    }}
+    if (playerSelection == "scissors") {
+        switch (computerSelection) {
+        case "rock":
+            console.log(loseMessage);
+            break;
+        case "paper":
+            console.log(winMessage);
+            break;
+        case "scissors":
+            console.log(equaMessage);
             break;
         default:
             console.log(defaultMessage);

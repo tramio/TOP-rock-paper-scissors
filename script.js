becdb8ae7c90ff6fe707a5de641f5e4e5311f079
Tramio <tramiodev@outlook.fr>
2021-07-21 15:56:35 +0200

    add loop  and round / scores counters

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const gameOptions = ["rock", "paper", "scissors"];
-let computerSelection;
-
-let playerSelection = prompt("Tell me your choice, adventurer!");
-playerSelection = playerSelection.toLowerCase();
-
-function computerPlay() {
-    computerSelection = gameOptions[Math.floor(Math.random()*gameOptions.length)];
-}
-
-computerPlay();
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
+
+    let playerSelection = prompt("Tell me your choice, adventurer!", "");
+    playerSelection = playerSelection.toLowerCase();
+
+    let computerSelection;
+    function computerPlay() {
+        computerSelection = gameOptions[Math.floor(Math.random()*gameOptions.length)];
+    }
+
+    computerPlay();
+
     let winMessage = "You won! " + playerSelection + " beats " + computerSelection + "!";
     let loseMessage = "You lost! " + computerSelection + " beats " + playerSelection + "!";
     let equaMessage = "It's a tie! Keep going!";
-    let defaultMessage = "I don't think I understood well, would you mind telling me again?"
+    let defaultMessage = "Your opponent broke! Someone must fix this game!";
 
     console.log("You went for " + playerSelection + ". Wise choice.","\n","Your opponent chose " + computerSelection + "! What a surprise!!!");
 
@@ -25,36 +28,42 @@ function playRound() {
             break;
         case "paper":
             console.log(loseMessage);
+            computerScore += 1;
             break;
         case "scissors":
             console.log(winMessage);
+            playerScore += 1;
             break;
         default:
             console.log(defaultMessage);
             break;
     }}
-    if (playerSelection == "paper") {
+    else if (playerSelection == "paper") {
         switch (computerSelection) {
         case "rock":
             console.log(winMessage);
+            playerScore += 1;
             break;
         case "paper":
             console.log(equaMessage);
             break;
         case "scissors":
             console.log(loseMessage);
+            computerScore += 1;
             break;
         default:
             console.log(defaultMessage);
             break;
     }}
-    if (playerSelection == "scissors") {
+    else if (playerSelection == "scissors") {
         switch (computerSelection) {
         case "rock":
             console.log(loseMessage);
+            computerScore += 1;
             break;
         case "paper":
             console.log(winMessage);
+            playerScore += 1;
             break;
         case "scissors":
             console.log(equaMessage);
@@ -65,4 +74,22 @@ function playRound() {
     }}
 }
 
-playRound();
+for (let round = 1; round < 6; round++) {
+    playRound();
+    let finalNarration = "This was round " + round + ". You: " + playerScore + ". Your opponent: " + computerScore + ".";
+    console.log(finalNarration);
+}
+
+function showWinner() {
+    if (playerScore == computerScore) {
+        console.log("Well, it's a draw.");
+    }
+    else if (playerScore > computerScore) {
+        console.log("You won this game!");
+    }
+    else {
+        console.log("You lost this game!");
+    }
+}
+
+showWinner();
